5c36cd0195c9d604f85c301342e90f53a7be6e78
devp2mc <devp2@mcmaster.ca>
2023-07-24 01:08:41 -0400

    A1

--- ATM_Bank_a.c
+++ ATM_Bank_a.c
@@ -1,10 +1,14 @@
 # include <stdio.h>
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 int main() 
 =======
 int main() //Main function contains all code
 >>>>>>> 3d8ce79 (A1 Final)
+=======
+int main() //Main function contains all body of code 
+>>>>>>> 656fc0d (A1)
 {
   printf("Hello");
   return 0;
